e29bd938fa0d9e32f2c213766d98345b2fd04c15
benorg <benorg@me.com>
2018-05-30 01:53:51 -0400

    follower scrolling cap, profile url

--- instagram.js
+++ instagram.js
@@ -28,44 +28,47 @@ function initClient(username, password) {
 }
 
 //returns a promise resolved with the profile info
-function scrapeProfile(handle) {
+function scrapeProfile(handle, followerCap) {
 
     return new Promise ((res, err) => {
         if (!session) err('Client not initialized');
 
         let info = { handle: handle, followers: [] };
-
         
         Client.Account.searchForUser(session, handle).then(user => {
 
             let feed = new Client.Feed.AccountFollowers(session, user.id);
     
-            Promise.all([
-                
+            new Promise((doneScrolling) => {
+
                 Client.Account.getById(session, user.id).then(data => {
                     data = data.params;
             
-                    info.numFollowers = data.followerCount;
+                    info.followerNum = data.followerCount;
                     info.bio = data.biography;
                     info.verified = data.isVerified;
                     info.isBusiness = data.isBusiness;
-                    data.externalUrl = data.externalUrl;
+                    info.externalUrl = data.externalUrl;
                     info.profilePic = data.profilePicUrl;
-                    info.numFollowing = data.followingCount;
+                    info.followingNum = data.followingCount;
                     info.igId = user.id;
                     info.name = data.fullName;
                     info.email = data.publicEmail;
                     info.publicPhone = data.publicPhoneNumber;
                     info.contactPhone = data.contactPhoneNumber;
-        
-                }, err),
 
-                new Promise((doneScrolling) => {
+                    //also doesn't scroll if no cap is provided
+                    if (info.followerNum <= followerCap)
+                        scroll();
+                    else {
+                        info.followers = 'Too many';
+                        doneScrolling();
+                    }
 
                     function scroll() {
 
                         feed.get().then(followers => {
-    
+        
                             for (var i = 0; i < followers.length; i++) {
                                 let cur = followers[i]._params;
                                 info.followers.push({
@@ -76,20 +79,19 @@ function scrapeProfile(handle) {
                                     verified: cur.isVerified,
                                 });
                             }
-    
+        
                             if (feed.moreAvailable)
                                 scroll();
                             else
                                 doneScrolling();
-
     
+        
                         }, err);
                     }
+        
+                }, err);
 
-                    scroll();
-                })
-
-            ]).then(() => res(info), err);
+            }).then(() => res(info), err);
     
         }, err);
 
@@ -97,4 +99,4 @@ function scrapeProfile(handle) {
 }
 
 
-initClient('fafsfs', 'fsaff').then(() => scrapeProfile('coltranetunes')).then(console.log, console.log);
+initClient('afaf', 'adfaf').then(() => scrapeProfile('instagram', 30000)).then(console.log);
